fc149288ed21c2bfafe9e0d5a2c34894afcbe86c
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2022-10-16 20:20:37 +0200

    Add docs

--- pacman/GameState.py
+++ pacman/GameState.py
@@ -3,11 +3,14 @@ from typing import List, Dict, Any, Set, Tuple
 from .Position import Position
 
 
+"""
+All info you might need, I hope.
+"""
 @dataclass
 class GameState:
-    you: Dict[str, Any]
-    other_pacmans: List[Dict[str, Any]]
-    ghosts: List[Dict[str, Any]]
+    you: Dict[str, Any]  # {'position': position, 'is_eatable': is_eatable, 'eatable_timer': eatable_timer}
+    other_pacmans: List[Dict[str, Any]]  # same as above but a list
+    ghosts: List[Dict[str, Any]]  # {'position': position, 'is_eatable': is_eatable, 'eatable_timer': eatable_timer, direction': direction}
     points: Set[Position]
     big_points: Set[Position]
     walls: Set[Position]
--- pacman/Pacman.py
+++ pacman/Pacman.py
@@ -3,20 +3,38 @@ from abc import ABC, abstractmethod
 from .Direction import Direction
 
 
+"""
+A pacman is that yellow thing with a big mouth that can eat points and ghosts!
+In this game, there can be more than one pacman and they can eat each other too.
+"""
 class Pacman(ABC):
+    """
+    Make your choice!
+    You can make moves completely randomly if you want, the game won't allow you to make an invalid move.
+    That's what invalid_move is for - it will be true if your previous choice was invalid.
+    """
     @abstractmethod
     def make_move(self, game_state, invalid_move=False) -> Direction:
         pass
 
+    """
+    The game will call this once for each pacman at each time step.
+    """
     @abstractmethod
     def give_points(self, points):
         pass
 
+    """
+    Do whatever you want with this info. The game will continue until all pacmans die or all points are eaten.
+    """
     @abstractmethod
     def on_death(self):
         pass
 
 
+"""
+I hope yours will be smarter than this one...
+"""
 class RandomPacman(Pacman):
     def give_points(self, points):
         print(f"random pacman got {points} points")
