81302fdf685e19e0c388777210eb99ad113d1a35
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2022-11-30 01:10:19 +0100

    Remove outdated comment

--- pacman/GameState.py
+++ pacman/GameState.py
@@ -2,15 +2,16 @@ from dataclasses import dataclass
 from typing import List, Dict, Any, Set, Tuple
 from .Position import Position
 
-
 """
 All info you might need, I hope.
 """
+
+
 @dataclass
 class GameState:
-    you: Dict[str, Any]  # {'position': position, 'is_eatable': is_eatable, 'eatable_timer': eatable_timer}
-    other_pacmans: List[Dict[str, Any]]  # same as above but a list
-    ghosts: List[Dict[str, Any]]  # {'position': position, 'is_eatable': is_eatable, 'eatable_timer': eatable_timer, direction': direction}
+    you: Dict[str, Any]
+    other_pacmans: List[Dict[str, Any]]
+    ghosts: List[Dict[str, Any]]
     points: Set[Position]
     big_points: Set[Position]
     walls: Set[Position]
